dbf81040c80b8d70d8e283d80dd8e03937f60ada
MarErius <92733280+MarErius@users.noreply.github.com>
2021-10-20 23:21:48 +0200

    Update registration.py.py

--- registration.py.py
+++ registration.py.py
@@ -2,14 +2,19 @@
 import hashlib
 
 def main():
+    #make user put in what they want as username and password
     uname = input("Please provide your username?")
     print("welcome,",uname)
     pword = input("Please choose a password")
-
+    
+    #encodes password so we can hash it
     penc = pword.encode()
+    #hash it as sha512
     pcrypt = hashlib.sha512()
     pcrypt.update(penc)
+    #open txt file we want to write the user to
     shadow = open("shadow.txt", "a")
+    #we need to use hashdigest() to get the hash.
     shadow.write((uname+":"+pcrypt.hexdigest()+":"))
     shadow.close()
     print("Congratulations! Your registration is completed!")
